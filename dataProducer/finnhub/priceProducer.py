52bd9052bc906e048a9091fd219a66ae8db50add
Daniel <danielsamg11@gmail.com>
2023-07-24 20:57:52 +0200

    updates

--- dataProducer/finnhub/priceProducer.py
+++ dataProducer/finnhub/priceProducer.py
@@ -1,7 +1,7 @@
 # from asyncio import create_task, get_event_loop, AbstractEventLoop,Task
 # import websocket
 # import json
-# from typing import List
+from typing import List
 # from os import environ
 
 
@@ -52,7 +52,7 @@
 #         "symbols",
 #         "start",
 #         "assets_objects",
-#         "ws"
+#         "ws",
 #         "task"
 #     ]
 
@@ -63,31 +63,35 @@
 #         self.symbols: List[str] = symbols
 #         self.assets_objects: dict = {}
 #         self.start: bool = False
-#         websocket.enableTrace(True)
-#         self.task: Task = []
+
+
 
 
 #     def run(self) -> None:
 #         self.start = True
+#         websocket.enableTrace(True)
 #         assets: list[str] = [x.upper() for x in self.symbols]
 #         for asset in assets:
 #             self.assets_objects[asset] = Asset(ticker=asset)
 #         self.ws = websocket.WebSocketApp(f"wss://ws.finnhub.io?token={self.__key}",
 #                               on_message = self.on_message,
 #                               on_error = self.on_error,
-#                               on_close = self.on_close)
+#                               on_close = self.on_close,
+#                               )
+#         self.ws.on_open = self.on_open
 #         self.ws.run_forever()
 
-#     def on_open(self):
+#     def on_open(self,ws):
 #         for asset in self.symbols:
 #             self.ws.send(json.dumps({"type": "subscribe", "symbol": asset}))
+#             print(f'Subscription for {asset} succeeded')
 
 #     def on_message(self,ws , message) -> None:
 #         message = json.loads(message)
 #         if message['type'] == 'trade':
-#             self.task: Task[None] = create_task(self._process_data(message=message))
-
-#         print(message)
+#             # self.task: Task[None] = create_task(self._process_data(message=message))
+#             # self.assets_objects[message['data']]
+#             print(message)
 
 #     def on_error(self, ws, error) -> None:
 #         print(error)
@@ -104,15 +108,15 @@
 #    obj = PriceProducer(symbols=['MSFT', 'APPLE'])
 #    obj.run()
 
-from asyncio import create_task, get_event_loop, AbstractEventLoop,Task,Queue,run
-import websockets
-import json
-from typing import List
-from os import environ
-import redis
-from os import environ
-from dotenv import load_dotenv
-load_dotenv()
+# from asyncio import create_task, get_event_loop, AbstractEventLoop,Task,Queue,run
+# import websockets
+# import json
+# from typing import List
+# from os import environ
+# import redis
+# from os import environ
+# from dotenv import load_dotenv
+# load_dotenv()
 
 
 class Asset:
@@ -152,58 +156,126 @@ class Asset:
         pass
 
 
+# class PriceProducer:
+
+#     __slots__: List[str] = [
+#         "__key",
+#         "symbols",
+#         "assets_objects",
+#         "queue",
+#         "redis_client"
+#     ]
+
+#     def __init__(self, symbols: List[str]) -> None:
+#         print('Initializing PriceProducer')
+#         self.__key: str = environ.get("FINNHUB_KEY")
+#         if not symbols:
+#             raise Exception("assets list is empty")
+#         self.symbols: List[str] = symbols
+#         self.assets_objects: dict = {}
+#         self.queue: Queue = Queue()
+#         self.redis_client = redis.Redis(host='redis', port=6379, db=0,password=environ.get('REDIS_PASSWORD'))
+#         if not self.redis_client.exists('add_tickers'):
+#             self.redis_client.lpush('add_tickers','')
+#             print('add tickers key created in redis')
+#         print('PriceProducer initialized')
+
+#     async def start(self) -> None:
+#         print('Starting PriceProducer')
+#         assets: list[str] = [x.upper() for x in self.symbols]
+#         for asset in assets:
+#             self.assets_objects[asset] = Asset(ticker=asset)
+#         consumer_task: Task = create_task(self.consumer_handler())
+#         await self.producer_handler()
+#         await consumer_task
+#         print('PriceProducer started')
+
+#     async def producer_handler(self)-> None:
+#         while True:  # Add a loop to retry the connection
+#             try:
+#                 async with websockets.connect(f"wss://ws.finnhub.io?token={self.__key}") as websocket:
+#                     for asset in self.symbols:
+#                         await websocket.send(json.dumps({"type": "subscribe", "symbol": asset}))
+#                     while True:
+#                         message = await websocket.recv()
+#                         await self.queue.put(message)
+#             except websockets.exceptions.ConnectionClosedError:
+#                 print("Connection closed, retrying...")
+#                 continue  # Retry the connection
+
+#     async def consumer_handler(self):
+#         while True:
+#             message = await self.queue.get()
+#             message = json.loads(message)
+#             if message['type'] == 'trade':
+#                 await self._process_data(message)
+
+#     async def _process_data(self, message: dict) -> None:
+#         asset: Asset = self.assets_objects[message['s']]
+#         await asset.extract(new_values=message['data'])
+
+
+# if __name__ == "__main__":
+#     try:
+#         print('run data producer')
+#         runner = PriceProducer(symbols=['MSFT', 'APPLE'])
+#         run(runner.start())
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+
+import asyncio
+import websockets
+import json
+from typing import List
+from os import environ
+# from task import create_task  # make sure this import is correct
+
 class PriceProducer:
 
     __slots__: List[str] = [
         "__key",
         "symbols",
+        "start",
         "assets_objects",
-        "queue",
-        "redis_client"
+        "ws",
+        "task"
     ]
 
-    def __init__(self, symbols: List[str] = []) -> None:
+    def __init__(self,symbols: List[str]=[]) -> None:
         self.__key: str = environ.get("FINNHUB_KEY")
         if not symbols:
             raise Exception("assets list is empty")
         self.symbols: List[str] = symbols
         self.assets_objects: dict = {}
-        self.queue: Queue = Queue()
-        self.redis_client = redis.Redis(host='redis', port=6379, db=0,password=environ.get('REDIS_PASSWORD'))
-        if not self.redis_client.exists('add_tickers'):
-            self.redis_client.lpush('add_tickers','')
-            print('add tickers key created in redis')
+        self.start: bool = False
 
-
-    async def start(self) -> None:
+    async def run(self) -> None:
+        self.start = True
         assets: list[str] = [x.upper() for x in self.symbols]
         for asset in assets:
             self.assets_objects[asset] = Asset(ticker=asset)
-        consumer_task: Task = create_task(self.consumer_handler())
-        await self.producer_handler()
-        await consumer_task
+        async with websockets.connect(f"wss://ws.finnhub.io?token={self.__key}") as self.ws:
+            await self.on_open()
+            async for message in self.ws:
+                task = asyncio.create_task(self.on_message(message))
+                # await self.on_message(message)
+                await task
+    async def on_open(self):
+        for asset in self.symbols:
+            await self.ws.send(json.dumps({"type": "subscribe", "symbol": asset}))
+            print(f'Subscription for {asset} succeeded')
 
-    async def producer_handler(self)-> None:
-        async with websockets.connect(f"wss://ws.finnhub.io?token={self.__key}") as websocket:
-            for asset in self.symbols:
-                await websocket.send(json.dumps({"type": "subscribe", "symbol": asset}))
-            while True:
-                message = await websocket.recv()
-                await self.queue.put(message)
-
-    async def consumer_handler(self):
-        while True:
-            message = await self.queue.get()
-            message = json.loads(message)
-            if message['type'] == 'trade':
-                await self._process_data(message)
+    async def on_message(self, message) -> None:
+        message = json.loads(message)
+        # if message['type'] == 'trade':
+            # await self._process_data(message=message)
+        print(message)
 
     async def _process_data(self, message: dict) -> None:
         asset: Asset = self.assets_objects[message['s']]
-        await asset.extract(new_values=message['data'])
+        asset.extract(new_values=message)
 
 
-if __name__ == "__main__":
-    print('run data producer')
-    obj = PriceProducer(symbols=['MSFT', 'APPLE'])
-    run(obj.start())
+if __name__ =="__main__":
+    obj = PriceProducer(symbols=['AAPL','MSFT'])
+    asyncio.run(obj.run())
